8d76d82d26f8785f55ec322844109eb15d3be4e2
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-07-01 10:00:25 +0200

    Update EquipmentsMonitoring.js

--- components/EquipmentsMonitoring.js
+++ components/EquipmentsMonitoring.js
@@ -41,7 +41,7 @@ function EquipmentList() {
           style={{
             position: "fixed",
             top: 0, left: 0, right: 0, bottom: 0,
-            backgroundColor: "rgba(0,0,0,0.5)",
+            backgroundColor: "rgba(1,1,1,0.5)",
             display: "flex",
             justifyContent: "center",
             alignItems: "center"
